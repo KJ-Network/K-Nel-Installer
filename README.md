7857be8a2445b9e5ceb09bcae0b589fa1bec7baa
knkj55 <knkj55@proton.me>
2025-08-02 08:46:34 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Kernel Installer
+---
+###### Install Android Kernel in Recovery/Magisk/KernelSU/APatch with K.I.S.S.:
+* Keep
+* It
+* Simple
+* Stupid.
+* For Flash Android Kernel Only.
+---
+##### Usage:
+* Put your kernel (Image, Image.gz, Image-dtb or Image.gz-dtb) into this package.
+* Put your dtb (*.dtb) into this package (if you have).
+* Change The Kernel Name and Device Codename in config.conf
+* Flash it in Recovery(3rd-party recoverys only), Magisk(as a module), KernelSU(as a module) or APatch(as a apm module)!
+---
+##### Credits:
+* [Magisk](https://github.com/topjohnwu/Magisk): We used magiskboot, util_functions and busybox from magisk here.
+* [Anykernel3](https://github.com/osm0sis/AnyKernel3): Where the idea from.
---
+++ customize.sh
@@ -0,0 +1,71 @@
+# Simple Kernel Installer
+# By KeJia
+
+# Get Kernel Name and print
+name=$(grep '^name=' $MODPATH/config.conf | cut -d '=' -f 2)
+
+ui_print " "
+ui_print "- $name Installer"
+ui_print " "
+
+# Get codename and check
+codename=$(grep '^codename=' $MODPATH/config.conf | cut -d '=' -f 2)
+
+if test -z "$(grep $codename /system/build.prop)"; then 
+    ui_print "- Device is not ' $codename ' , cannot install this kernel."
+    exit 1
+fi
+
+# Get boot partition name
+if [ ! -e /dev/block/bootdevice/by-name/boot* ]; then
+    ui_print "- Unsupport Environment!"
+    exit 1
+elif test -z "$(getprop ro.boot.slot_suffix)"; then
+    export boot="/dev/block/bootdevice/by-name/boot"
+else
+    export boot="/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix)"
+fi
+
+# Install Kernel
+chmod +x $MODPATH/tools/*
+export PATH="$MODPATH/tools:$PATH"
+
+ui_print "- Extracting 'Boot' Image..."
+dd if=$boot of=$MODPATH/boot.img
+ui_print "- Unpacking 'Boot' Image..."
+magiskboot unpack $MODPATH/boot.img
+ui_print "- Spliting The Kernel Package And Uploading The Kernel..."
+if [ -e $MODPATH/Image.gz-dtb ]; then
+    magiskboot split $MODPATH/Image.gz-dtb
+elif [ -e $MODPATH/Image-dtb ]; then
+    magiskboot split $MODPATH/Image-dtb
+elif [ -e $MODPATH/Image.gz ]; then
+    magiskboot decompress $MODPATH/Image.gz kernel
+elif [ -e $MODPATH/Image ]; then
+    mv $MODPATH/Image kernel
+else
+    ui_print "- No kernel Image detected! Install failed!"
+    exit 1
+fi
+if [ -e $MODPATH/*.dtb ]; then
+    mv $MODPATH/*.dtb kernel_dtb
+fi
+ui_print "- Repacking 'Boot' Image..."
+magiskboot repack $MODPATH/boot.img
+ui_print "- Flashing 'Boot' Image..."
+dd if=new-boot.img of=$boot
+ui_print "- Install Successful!"
+
+ui_print " "
+
+ui_print "- Cleaning GPU cache..."
+find /data/user_de/*/*/*cache/* -iname "*shader*" -exec rm -rf {} +
+find /data/data/* -iname "*shader*" -exec rm -rf {} +
+find /data/data/* -iname "*graphitecache*" -exec rm -rf {} +
+find /data/data/* -iname "*gpucache*" -exec rm -rf {} +
+find /data_mirror/data*/*/*/*/* -iname "*shader*" -exec rm -rf {} +
+find /data_mirror/data*/*/*/*/* -iname "*graphitecache*" -exec rm -rf {} +
+find /data_mirror/data*/*/*/*/* -iname "*gpucache*" -exec rm -rf {} +
+ui_print "- GPU Cache Cleaning Completed."
+
+ui_print " "
